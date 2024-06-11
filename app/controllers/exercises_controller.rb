fdc8be395844d2a2e2bad81610cc263b2d1a0e7f
eri <k.erii0341115@gmail.com>
2024-06-11 14:32:56 +0900

    updata

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -3,6 +3,9 @@ class ExercisesController < ApplicationController
     # 【要件】注文されていないすべての料理を返すこと
     #   * left_outer_joinsを使うこと
     @foods = Food.left_outer_joins(:orders)
+    @foods .each do |food|
+      puts.food.name
+
 
   end
 
@@ -16,14 +19,14 @@ class ExercisesController < ApplicationController
     # 【要件】配達先の一番多い住所を返すこと
     #   * joinsを使うこと
     #   * 取得したAddressのインスタンスにorders_countと呼びかけると注文の数を返すこと
-    @address = Address.joins(:customers)
-    #Customer.orders_count(:)
+    @address = Co.joins(:customers)
+    Customer.orders_count(:)
   end
 
   def exercise4 
     # 【要件】一番お金を使っている顧客を返すこと
     #   * joinsを使うこと
     #   * 取得したCustomerのインスタンスにfoods_price_sumと呼びかけると合計金額を返すこと
-    #@customer = Customer.joins(:)
+    @customer = Customer.joins(:)
   end
 end
