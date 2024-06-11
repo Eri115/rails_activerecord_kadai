09c8e91508a7af8dad2732fe2c7439632256b3bd
eri <k.erii0341115@gmail.com>
2024-06-11 17:29:59 +0900

    updata

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -2,25 +2,26 @@ class ExercisesController < ApplicationController
   def exercise1
     # 【要件】注文されていないすべての料理を返すこと
     #   * left_outer_joinsを使うこと
-    @foods = Food.left_outer_joins(:orders).where(orders: {id: nill})
-    @foods.each do |food|
-      puts food.name
-
+    @foods = Food.left_outer_joins(:orders).where(orders: {id: nil })
 
   end
 
   def exercise2
     # 【要件】注文されていない料理を提供しているすべてのお店を返すこと
     #   * left_outer_joinsを使うこと
-    @shops = Shop.left_outer_joins(:order_foods)
-    #@shops.each.do |shop|
+  
+    @shops = Shop.left_outer_joins(:order_foods).where(orders: {id: order_foods})
+    @shops.each.do |shop|
+      puts shop.name
+    #　料理とお店を結合する
+    #　注文されていない料理を提供してる全てのお店を探す
   end
 
   def exercise3 
     # 【要件】配達先の一番多い住所を返すこと
     #   * joinsを使うこと
     #   * 取得したAddressのインスタンスにorders_countと呼びかけると注文の数を返すこと
-   #@address = Co.joins(:customers)
+    @address = address.joins(:orders).group("city").
     #Customer.orders_count(:)
   end
 
